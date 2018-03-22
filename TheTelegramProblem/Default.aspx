47fdd5d23d3ce29287e007fdc6f75a0929df0435
Zachary Velcoff <33340277+zackfactual@users.noreply.github.com>
2018-03-22 11:49:58 -0700

    Initial commit

---
+++ TheTelegramProblem/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TheTelegramProblem.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title>The Telegram Problem</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+		<div>
+    		<h1>The Telegram Problem</h1>
+			<p style="font-style:italic;">Given an integer (X) and a string containing no words longer than X characters, output that string in lines no longer than X characters without breaking any word across multiple lines.</p>
+			<p>
+				Enter an integer: 
+				<asp:TextBox ID="integerTextBox" runat="server"></asp:TextBox>
+			</p>
+			<p>
+				Enter a string:
+				<asp:TextBox ID="stringTextBox" runat="server"></asp:TextBox>
+			</p>
+			<asp:Button ID="goButton" runat="server" OnClick="goButton_Click" Text="Go" />
+			<br />
+			<br />
+			<asp:Label ID="resultLabel" runat="server"></asp:Label>
+		</div>
+    </form>
+</body>
+</html>
---
+++ TheTelegramProblem/Default.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TheTelegramProblem
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void goButton_Click(object sender, EventArgs e)
+		{
+			// reset resultLabel
+			resultLabel.Text = "";
+			int lineLimit;
+			string line = "";
+
+			// read character limit of lines from integer text box
+			if (int.TryParse(integerTextBox.Text, out lineLimit))
+			{
+				// read string from stringTextBox
+				if(stringTextBox.Text.Trim().Length != 0)
+				{
+					// split string into an array of words by spaces
+					string[] wordsInString = stringTextBox.Text.Split(' ');
+
+					foreach (var word in wordsInString)
+					{
+						// throw exception if any word in the string is longer than the lineLimit
+						if (word.Length > lineLimit)
+						{
+							resultLabel.Text = String.Format("Please enter a string with no words longer than {0} characters!",
+								lineLimit);
+							return;
+						}
+						// if line length + word length <= lineLimit, add word to line
+						else if ((line.Length + word.Length) <= lineLimit)
+						{
+							line += (word + " ");
+						}
+						// if line length + word length > lineLimit, break and display line, then reset line to word
+						else if ((line.Length + word.Length) > lineLimit)
+						{
+							line += "<br />";
+							resultLabel.Text += line;
+							line = (word + " ");
+						}
+					}
+					// add last word in string
+					resultLabel.Text += wordsInString[wordsInString.Length - 1];
+				}
+				// throw exception if no string or integer is input by the user
+				else
+				{
+					resultLabel.Text = "Please enter a valid string!";
+				}
+			}
+			else
+			{
+				resultLabel.Text = "Please enter a valid integer!";
+			}
+		}
+	}
+}
